83e31a211e80421e511160974d448295620489b5
Aleksandar Vladimirov <aleksandar.vladimirov.a63@learn.telerikacademy.com>
2024-10-11 15:40:13 +0300

    -added credentials.txt to the gitignore file - added default DB connection credentials to the credentials_ready.py - and cleaned up the old code

--- credentials_reader/credentials_reader.py
+++ credentials_reader/credentials_reader.py
@@ -1,33 +1,12 @@
-# def credentials_reader():
-#     path = r'C:\Users\alexa\Desktop\credentials.txt'  # <- Тук вкарайте пътя към вашия credentials.txt!!!
-#     credentials = {'user': None, 'password': None, 'host': None, 'port': None, 'database': None}
-#     keys = list(credentials.keys())
-#
-#     with open(path, 'r') as file:
-#         content = file.readlines()
-#
-#         for n, line in enumerate(content):
-#             line = line.strip()
-#             if n < len(keys):
-#                 if keys[n] == 'port':
-#                     credentials[keys[n]] = int(line)
-#                 else:
-#                     credentials[keys[n]] = line
-#
-#     return credentials
-#
-#
-# print(credentials_reader())
-
 import os
 
 def credentials_reader():
     # Get the current script's directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # Go two directories up from 'forum-api' to find 'credentials.txt'
-    grandparent_dir = os.path.abspath(os.path.join(current_dir))  # Go up two levels
-    path = os.path.join(grandparent_dir, 'credentials.txt')
+
+    containing_dir = os.path.abspath(os.path.join(current_dir))
+    path = os.path.join(containing_dir, 'credentials.txt')
 
     # Default credentials
     credentials = {'user': 'root',
@@ -36,8 +15,7 @@ def credentials_reader():
                    'port': 3306,
                    'database': 'forum_3'}
 
-
-    # Check if the file exists two directories up
+    # Credentials pull
     if os.path.exists(path):
         with open(path, 'r') as file:
             content = file.readlines()

d5771ba56f957a49f24c1d88db7c7284328410cd
alexis19537 <alexander.vankov03@gmail.com>
2024-10-16 16:00:21 +0300

    routers.categories.py:  - {GET} get_all_categories()  - {GET} get_category_by_id()  - {POST} create_category()  - {PUT} lock_category()  - {PUT} unlock_category()  - {PUT} make_category_private()  - {PUT} make_category_public()
     service.category_service:
      - get_categories()
      - get_by_id()
      - create()
      - check_if_locked()
      - check_if_private()
      - lock()
      - unlock()
      - make_private()
      - make_public()

--- routers/categories.py
+++ routers/categories.py
@@ -1,3 +1,55 @@
 from fastapi import APIRouter
+from service import categories_service
+from common.responses import NotFound
 
 cat_router = APIRouter(prefix="/categories", tags=["Categories"])
+
+
+@cat_router.get('')
+def get_all_categories():
+
+    data = categories_service.get_categories()
+
+    return data
+
+
+@cat_router.get('/{id}')
+def get_category_by_id(id: int):
+    category = categories_service.get_by_id(id)
+
+    if category is None:
+        return NotFound()
+    else:
+        return category
+
+
+@cat_router.post('')
+def create_category(cat_name: str, creator_id: int):
+    category = categories_service.create(cat_name, creator_id)
+
+    return category
+
+
+@cat_router.put('/lock/{id}')
+def lock_category(id: int):
+    categories_service.lock(id)
+    return get_category_by_id(id)
+
+
+@cat_router.put('/make_private/{id}')
+def make_category_private(id: int):
+    categories_service.make_private(id)
+    return get_category_by_id(id)
+
+
+@cat_router.put('/unlock/{id}')
+def unlock_category(id: int):
+    categories_service.unlock(id)
+    return get_category_by_id(id)
+
+
+@cat_router.put('/make_public/{id}')
+def make_category_public(id: int):
+    categories_service.make_public(id)
+    return get_category_by_id(id)
+
--- service/categories_service.py
+++ service/categories_service.py
@@ -1,2 +1,64 @@
-# from data.models import ...
-# from data.database import ...
+from data.database import insert_query, read_query, update_query
+from data.models import Category
+from common.responses import BadRequest
+
+
+def get_categories():
+    data = read_query('select * from categories order by id')
+
+    return (Category(id=id, cat_name=cat_name, creator_id=creator_id, is_locked=is_locked, is_private=is_private) for id, cat_name, creator_id, is_locked, is_private in data)
+
+
+def get_by_id(id: int):
+    data = read_query('select * from categories where id = ?', (id,))
+
+    return next((Category(id=id, cat_name=cat_name, creator_id=creator_id, is_locked=is_locked, is_private=is_private)
+                 for id, cat_name, creator_id, is_locked, is_private in data), None)
+
+
+# def exists(id: int):
+#     return any(
+#         read_query(
+#             'select id, name from categories where id = ?',
+#             (id,)))
+#
+
+def create(cat_name, creator_id):
+    generated_id = insert_query(
+        'insert into categories(cat_name, creator_id) values(?,?)',
+        (cat_name, creator_id))
+    return get_by_id(generated_id)
+
+
+def check_if_locked(id: int):
+    data = read_query('select is_locked from categories where id = ?', (id,))
+    return next((is_locked for is_locked, in data), None)
+
+
+def check_if_private(id: int):
+    data = read_query('select is_private from categories where id = ?', (id,))
+    return next((is_private for is_private, in data), None)
+
+
+def lock(id: int):
+    if check_if_locked(id):
+        return BadRequest('Category is already locked.')
+    update_query('update categories set is_locked = 1 where id = ?', (id,))
+
+
+def make_private(id: int):
+    if check_if_private(id):
+        return BadRequest('Category is already private.')
+    update_query('update categories set is_private = 1 where id = ?', (id,))
+
+
+def unlock(id: int):
+    if not check_if_locked(id):
+        return BadRequest('Category is already unlocked.')
+    update_query('update categories set is_locked = 0 where id = ?', (id,))
+
+
+def make_public(id: int):
+    if not check_if_private(id):
+        return BadRequest('Category is already public.')
+    update_query('update categories set is_private = 0 where id = ?', (id,))

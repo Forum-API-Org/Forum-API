73433481fac2e032adc4125aeae1b7489fd3a557
alexis19537 <alexander.vankov03@gmail.com>
2024-10-16 11:37:00 +0300

    Revert "Update credentials_reader.py"
    This reverts commit 6e287b363c78d414f1f7f342f47c7cb22835a8f4.

--- credentials_reader/credentials_reader.py
+++ credentials_reader/credentials_reader.py
@@ -1,18 +1,35 @@
+import os
+
 def credentials_reader():
-    path = r'C:\Users\alexa\Desktop\credentials.txt'  # <- Тук вкарайте пътя към вашия credentials.txt!!!
-    credentials = {'user': None, 'password': None, 'host': None, 'port': None, 'database': None}
-    keys = list(credentials.keys())
+    # Get the current script's directory
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    with open(path, 'r') as file:
-        content = file.readlines()
 
-        for n, line in enumerate(content):
-            line = line.strip()
-            if n < len(keys):
-                if keys[n] == 'port':
-                    credentials[keys[n]] = int(line)
-                else:
-                    credentials[keys[n]] = line
+    containing_dir = os.path.abspath(os.path.join(current_dir))
+    path = os.path.join(containing_dir, 'credentials.txt')
+
+    # Default credentials
+    credentials = {'user': 'root',
+                   'password': 'root',
+                   'host': 'localhost',
+                   'port': 3306,
+                   'database': 'forum_3'}
+
+    # Credentials pull
+    if os.path.exists(path):
+        with open(path, 'r') as file:
+            content = file.readlines()
+
+            keys = list(credentials.keys())
+            for n, line in enumerate(content):
+                line = line.strip()
+                if n < len(keys):
+                    if keys[n] == 'port':
+                        credentials[keys[n]] = int(line)
+                    else:
+                        credentials[keys[n]] = line
+    else:
+        print(f"Warning: File at {path} not found. Using default credentials.")
 
     return credentials
 

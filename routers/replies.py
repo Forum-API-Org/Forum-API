fe8ea9a8834ffaf15abf8d7b73147d0f1d65c5f7
heijaz-smiled <alxander.dimitrov@gmail.com>
2024-10-16 19:58:34 +0300

    - endpoint post reply - server create reply - endpoint put vote - server vote -changed name of 'service' folder to 'sevices'

--- routers/replies.py
+++ routers/replies.py
@@ -1,3 +1,15 @@
 from fastapi import APIRouter
+from services import replies_service
+from data.models import Reply
 
 replies_router = APIRouter(prefix = "replies", tags = ["Replies"])
+
+@replies_router.post('/{topic_id}')
+def create_reply(reply: Reply ,topic_id: int):
+
+    #if user validation
+
+    reply.topic_id = topic_id
+
+    return replies_service.create(reply)
+
--- routers/votes.py
+++ routers/votes.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
+from services import votes_service
+from data.models import Vote
 
 votes_router = APIRouter(prefix="/votes", tags=["Votes"])
+
+@votes_router.put('/{reply_id}')
+def put_vote(vote: Vote):
+
+    return votes_service.vote(vote)

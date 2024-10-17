d3d59e510092328acaf6c252a04c97a75f4834bd
heijaz-smiled <alxander.dimitrov@gmail.com>
2024-10-17 15:32:35 +0300

    created replies - fixed typo

--- routers/replies.py
+++ routers/replies.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from services import replies_service
 from data.models import Reply
 
-replies_router = APIRouter(prefix = "replies", tags = ["Replies"])
+replies_router = APIRouter(prefix = "/replies", tags = ["Replies"])
 
 @replies_router.post('/{topic_id}')
 def create_reply(reply: Reply ,topic_id: int):

2552afc3b148b1758990bc356bfeb6c5dbc9ad66
Aleksandar Vladimirov <aleksandar.vladimirov.a63@learn.telerikacademy.com>
2024-11-07 12:51:37 +0200

    - previous version of messages restored

--- routers/messages.py
+++ routers/messages.py
@@ -1,20 +1,19 @@
 from fastapi import APIRouter, Header
-from data.models import MessageText
+from data.models import MessageText, Message, UserResponse
 from common.responses import BadRequest, NotFound
 from services import messages_service, users_service
-from typing import Annotated
+from typing import Annotated, List
 
-message_router = APIRouter(prefix="/messages", tags=["Messages"])
+message_router = APIRouter(prefix="/messages/users", tags=["Messages"])
 
-@message_router.post('/{receiver_id}')
-def create_message(receiver_id, msg: MessageText, token: Annotated[str, Header()]):
 
-    #if user authorization
+@message_router.post('/')
+def create_message(msg: MessageText, token: Annotated[str, Header()]):
+    # if user authorization
 
     receiver = users_service.get_user_by_id(msg.receiver_id)
     if not receiver:
         return NotFound(content="Receiver does not exist.")
-
 
     if not msg.text.strip():
         return BadRequest(content="Message text cannot be empty.")
@@ -22,7 +21,7 @@ def create_message(receiver_id, msg: MessageText, token: Annotated[str, Header()
     if len(msg.text) > 500:
         return BadRequest(content="Reply text cannot be more than 500 characters.")
 
-    message = messages_service.create(receiver_id, msg, token)
+    message = messages_service.create(msg, token)
 
     return message
 
@@ -31,9 +30,6 @@ def create_message(receiver_id, msg: MessageText, token: Annotated[str, Header()
 
 @message_router.get('/{receiver_id}')
 def view_conversation(receiver_id: int, token: Annotated[str, Header()]):
-
-
-
     receiver = users_service.get_user_by_id(receiver_id)
     if not receiver:
         return NotFound(content="Receiver does not exist.")
@@ -44,15 +40,15 @@ def view_conversation(receiver_id: int, token: Annotated[str, Header()]):
         return NotFound(content="No conversation found")
 
     return conversation
-    #return [message.dict(exclude={"id", "receiver_id"}) for message in result]
+    # return [message.dict(exclude={"id", "receiver_id"}) for message in result]
     # return {
     #     "conversation": [(r.sender_id, r.message_text) for r in result],
     #     "message_count": len(result)
     # }
 
+
 @message_router.get('/')
 def view_conversations(token: Annotated[str, Header()]):
-
     data = messages_service.all_conversations(token)
 
     if not data:
